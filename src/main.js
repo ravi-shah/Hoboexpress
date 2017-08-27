04fb59272a38638cf7989957aea81b1bc23ccc7e
Apcrat <apcrat@apcrats-mbp.gateway.sasktel.net>
2017-08-27 17:48:21 -0600

    add webpack

---
+++ src/main.js
@@ -0,0 +1,21 @@
+console.log("Hello world");
+
+/*import 'React' from 'react';
+import 'ReactDom' from 'react-dom';
+
+const App = (props) => {
+  return (
+    <div className="text-center">
+    <h2>
+      Hi, Hello React Component!
+    </h2>
+    {props.componentVariable}
+    </div>
+  )
+}
+
+ReactDom.render(
+  <App />,
+  document.getElementById('container');
+);
+*/
